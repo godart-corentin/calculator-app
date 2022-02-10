e8bf431bc2bb728455a1ecc8a5cca1b84e1a60e2
Corentin Godart <godartcn@gmail.com>
2022-02-10 20:26:30 +0100

    Base configuration (libraries)

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBackspace,
+  faDivide,
+  faMoon,
+  faMultiply,
+  faPlus,
+  faSubtract,
+  faSun,
+  faHand,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faSun,
+  faMoon,
+  faBackspace,
+  faDivide,
+  faMultiply,
+  faSubtract,
+  faPlus,
+  faHand
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
